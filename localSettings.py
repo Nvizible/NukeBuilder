7bb257110d14e10281513b769444fee29ff2bcd8
Hugh Macdonald <hugh.macdonald@nvizible.com>
2011-06-26 13:06:26 +0100

    Removed OSX subpath from localSettings. Now should just point to the Nuke installation folder

--- localSettings.py
+++ localSettings.py
@@ -3,8 +3,8 @@ defaultNukeVersion = "6.2v4"
 defaultArch = "64"
 
 nukeLocation = {
-                'darwin.i386'    : "/Applications/Nuke%(nuke_version)s-32/Nuke%(nuke_version)s.app/Contents/MacOS",
-                'darwin.x86_64'  : "/Applications/Nuke%(nuke_version)s/Nuke%(nuke_version)s.app/Contents/MacOS",
+                'darwin.i386'    : "/Applications/Nuke%(nuke_version)s-32",
+                'darwin.x86_64'  : "/Applications/Nuke%(nuke_version)s",
                 'linux.i386'     : "/usr/local/Nuke%(nuke_version)s",
                 'linux.x86_64'   : "/usr/local/Nuke%(nuke_version)s",
                 'windows.x86'    : "C:\\Program Files (x86)\\Nuke%(nuke_version)s",
