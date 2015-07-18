6f25249ee6474544f4012394c15366895a7e0acc
Hugh Macdonald <hugh.macdonald@nvizible.com>
2015-07-18 15:32:18 +0100

    Update to latest build settings

--- localSettings.py
+++ localSettings.py
@@ -1,12 +1,23 @@
 
-defaultNukeVersion = "6.2v4"
+defaultNukeVersion = "9.0v6"
 defaultArch = "64"
 
 nukeLocation = {
-                'darwin.i386'    : "/Applications/Nuke%(nuke_version)s-32",
-                'darwin.x86_64'  : "/Applications/Nuke%(nuke_version)s",
+                'darwin.i386'    : "/Applications/Nuke%(nuke_version)s-32/Nuke%(nuke_version)s.app/Contents/MacOS",
+                'darwin.x86_64'  : "/Applications/Nuke%(nuke_version)s/Nuke%(nuke_version)s.app/Contents/MacOS",
                 'linux.i386'     : "/usr/local/Nuke%(nuke_version)s",
                 'linux.x86_64'   : "/usr/local/Nuke%(nuke_version)s",
                 'windows.x86'    : "C:\\Program Files (x86)\\Nuke%(nuke_version)s",
                 'windows.x86_64' : "C:\\Program Files\\Nuke%(nuke_version)s"
                 }
+
+install = {
+            'debug': {
+                        'plugins' : "${HOME}/.nuke/",
+                        'menu'    : "${HOME}/.nuke/NV_DeepTools/%(nuke_build_version)s/"
+                     },
+            'release': {
+                        'plugins' : "${HOME}/.nuke/",
+                        'menu'    : "${HOME}/.nuke/NV_DeepTools/%(nuke_build_version)s/"
+                       }
+          }
